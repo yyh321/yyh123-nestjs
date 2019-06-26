53b91dd1a1397be19aed629cf75e388ef5764a37
yyh123 <yuyunhao1@126.com>
2019-06-27 00:33:11 +0800

    把TypeORM配置放在环境变量里（.env）

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,18 +5,7 @@ import { AppService } from './app.service';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot(
-      {
-        type:'mysql',
-        host:'localhost',
-        port:3306,
-        username:'nest',
-        password:'password',
-        database:'nest',
-        synchronize:true,
-        entities: [__dirname + '/**/*.entity{.ts,.js}']
-      }
-    )
+    TypeOrmModule.forRoot()
   ],
   controllers: [AppController],
   providers: [AppService],
